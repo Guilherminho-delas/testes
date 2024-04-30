86cf211e620bf167f2a1958bc540c1ba1c3804ae
Guilherminho-delas <161325878+Guilherminho-delas@users.noreply.github.com>
2024-04-30 18:41:51 -0300

    Create questao_60.c
    dosojfk

---
+++ questao_60.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+int main()
+{
+//var
+int inteiro;
+int primo1, primo2;
+int icont, jcont;
+
+//inicio
+
+primo1 = 0;
+primo2 = 0;
+for (inteiro = 500; inteiro <= 1000; inteiro += 2)
+{
+    
+    while (primo1 + primo2 != inteiro)
+    
+    
+    
+    
+          for (icont = 3; icont < 1000; icont++)
+              for(jcont = 2; jcont <= icont/2; jcont++)
+                  if (icont%jcont == 0)
+                     pri
+        
+}
+
+
+}
